be975ab924377b0238db9f537bcef1eecc293dd0
Amir <amirsharifico@gmail.com>
2023-01-04 13:07:02 -0500

    typo fixed

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ app.get('/', (req, res) =>{
     res.send('helooooooooo')
 })
 
-app.listen(3000, () => console.log('Listening to port 3000...'))
+app.listen(3000, () => console.log('Listening to- port 3000...'))
