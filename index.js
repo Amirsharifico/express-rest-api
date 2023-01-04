42e0295708c4e00a63605a8f24a0aad33fdcb5de
Amir <amirsharifico@gmail.com>
2023-01-04 11:34:41 -0500

    branch 1- initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app= express();
+
+app.get('/', (req, res) =>{
+    res.send('helooooooooo')
+})
+
+app.listen(3000, () => console.log('Listening tom port 3000...'))
+
