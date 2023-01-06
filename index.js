8364485440bd490f5fea6bf31499f49974e4f940
amirsharifico <amirsharifico@gmail.com>
2023-01-05 21:52:53 -0500

    Docker file created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app= express();
 
 app.get('/', (req, res) =>{
-    res.send('helooooooooo')
+    res.send('This message is from insid of express-rest-api app by A5HKIAN ')
 })
 
 app.listen(3000, () => console.log('Listening to port 3000...'))
